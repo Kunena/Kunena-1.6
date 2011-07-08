57a5398b78ce6862cfdc9316c316fc2d27214571
xillibit <xillibit@gmail.com>
2011-07-08 19:34:29 +0200

    # [#14] Category manager set item unpublished when filtered, close #14

--- components/com_kunena/CHANGELOG.php
+++ components/com_kunena/CHANGELOG.php
@@ -33,6 +33,9 @@ Legend:
 
 Kunena 1.6.5-DEV
 
+8-July-2011 Xillibit
+# [#14] Category manager set item unpublished when filtered
+
 7-July-2011 fxstein
 ^ [#10] Updated README.md for github (and builder)
 ^ [#9] Increase avatar database column to 255 - to support longer paths from conversions
