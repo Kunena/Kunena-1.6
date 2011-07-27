4a52ef57ce1a7c76c0498ef4518c325e7f4234e0
Matias Griese <matias@kunena.org>
2011-07-27 22:18:43 +0300

    ^ [#27] Change Dutch language

--- components/com_kunena/CHANGELOG.php
+++ components/com_kunena/CHANGELOG.php
@@ -33,6 +33,9 @@ Legend:
 
 Kunena 1.6.5-DEV
 
+27-july-2011 810
+^ [#27] Change Dutch language
+
 27-July-2011 Matias
 # [#27] Fixing broken language files (best effort)
 - [#27] Removing Macedonian language as it doesn't fit into package and hasn't been updated
