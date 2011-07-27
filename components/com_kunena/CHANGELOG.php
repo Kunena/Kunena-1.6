43bcbd3f3ac8ffbb599e50d5b276d4c39996e5c4
Matias Griese <matias@kunena.org>
2011-07-27 17:11:27 +0300

    - [#27] Remove Macedonian as it doesn't fit into package and not updated

--- components/com_kunena/CHANGELOG.php
+++ components/com_kunena/CHANGELOG.php
@@ -35,6 +35,7 @@ Kunena 1.6.5-DEV
 
 27-July-2011 Matias
 # [#27] Fixing broken language files (best effort)
+- [#27] Removing Macedonian as it doesn't fit into package and hasn't been updated
 
 26-July-2011 svanschu
 ^ [#27] pulling from transifex
