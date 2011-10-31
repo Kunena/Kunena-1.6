d983af398cae43572c22b54cb4778f1a169ea114
Matias Griese <matias@kunena.org>
2011-10-31 13:08:50 +0200

    [#169] Hide some extra information from configuration report (part 2)

--- components/com_kunena/CHANGELOG.php
+++ components/com_kunena/CHANGELOG.php
@@ -33,6 +33,9 @@ Legend:
 
 Kunena 1.7.1-DEV
 
+31-October-2011 Matias
+# [#169] Hide some extra information from configuration report (part 2)
+
 29-October-2011 Matias
 # [#168] UTF title issue
 # [#169] Hide some extra information from configuration report
