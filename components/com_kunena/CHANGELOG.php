8d6e67f09b71e2be16032cf330191b1366d40e37
Florian DAL FITTO <xillibit@gmail.com>
2012-01-20 21:14:27 +0100

    # [#232] Kunena report generator not to conceal menu items

--- components/com_kunena/CHANGELOG.php
+++ components/com_kunena/CHANGELOG.php
@@ -33,6 +33,9 @@ Legend:
 
 Kunena 1.7.2-DEV
 
+20-January-2012 Xillibit
+# [#232] Kunena report generator not to conceal menu items
+
 15-January-2012 Matias
 # [#222] CB integration login breaks user ACL (workaround Joomla 1.7.3 bug)
 # [#224] Bulk Actions post URL issues
